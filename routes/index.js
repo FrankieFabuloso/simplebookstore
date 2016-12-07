40a9c64968d8b3b80056b86e97925fc14d6c43a7
FrankieFabuloso <frocha@ucsc.edu>
2016-12-06 16:07:09 -0800

    Adds simple book details view

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bookstore' });
 });
 
+/* GET bookDetails */
+router.get('/book', function(req, res, next) {
+  res.render('bookDetails', { book: {title: "Cheeseballs", description: "This book is about Cheeseballs", average_rating: 4, authors: ["Mr. Cheese", "Mrs.Cheese"] }});
+});
+
 module.exports = router;
